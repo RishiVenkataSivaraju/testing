84e17b53687327f1b24b16176a9fd229f36fa4b4
Rishi Sivaraju <sivarajurishi@gmail.com>
2023-11-11 10:44:59 +0530

    Test Deploy

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require("path")
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'pubilc')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'styles')));
+
+
+app.get("/", (req, res) => {
+    res.render("app")
+})
+
+app.listen(3000, () => {
+    console.log("Serving on port 30000");
+})
+
