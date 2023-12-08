e2f3331d493e14d7e13a810cc8bc0d40c6a74b6b
Rishi Sivaraju <sivarajurishi@gmail.com>
2023-12-08 09:24:51 +0530

    NEW DEPLOY TRY ON AWS

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require("path")
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'pubilc')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'styles')));
 
